c4b20a5dbabe04aa0f9e4a58a4601c25f95a8f19
harishv207 <hvenkate@visa.com>
2021-05-30 17:32:41 +0800

    Finishing up todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,28 +1,80 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 export default function TodoList() {
-  const [todos, setTodos] = useState([
-    { title: 'Buy groceries' },
-    { title: 'Water the plants' },
-    { title: 'Complete Homework' },
-  ]);
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState(null);
+  const sampleTodos = [
+    { title: 'Buy groceries', isCompleted: false },
+    { title: 'Water the plants', isCompleted: false },
+    { title: 'Complete Homework', isCompleted: false },
+  ];
+
+  const saveTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  const getTodos = () => {
+    const newTodos = JSON.parse(localStorage.getItem('todos'));
+    if (newTodos && newTodos.length > 0) {
+      setTodos(newTodos);
+    } else {
+      setTodos(sampleTodos);
+    }
+  };
+
+  useEffect(() => {
+    if (todos.length === 0) {
+      getTodos();
+    }
+    saveTodos();
+  }, [todos]);
 
   const getTodosToRender = () => {
     return todos.map((todo, idx) => {
       return (
         <div className="columns todo mt-3 is-vcentered">
           <div className="column has-text-left">
-            <div key={idx}>{todo.title}</div>
+            <div
+              key={idx}
+              className={todo.isCompleted ? 'completed' : 'incomplete'}
+            >
+              {todo.title}
+            </div>
           </div>
           <div className="column is-narrow">
             <div className="buttons">
-              <button className="button is-success">Complete</button>
-              <button className="button is-danger">Delete</button>
+              {!todo.isCompleted ? (
+                <button
+                  className="button is-success"
+                  onClick={() => handleCompleteTask(idx)}
+                >
+                  Complete
+                </button>
+              ) : null}
+
+              <button
+                className="button is-danger"
+                onClick={() => handleDeleteTask(idx)}
+              >
+                Delete
+              </button>
             </div>
           </div>
         </div>
       );
     });
+  };
+
+  const handleDeleteTask = (idx) => {
+    const newTodos = [...todos];
+    newTodos.splice(idx, 1);
+    setTodos(newTodos);
+  };
+
+  const handleCompleteTask = (idx) => {
+    const newTodos = [...todos];
+    newTodos[idx].isCompleted = true;
+    setTodos(newTodos);
   };
 
   const handleInputChanged = (event) => {
@@ -31,7 +83,7 @@ export default function TodoList() {
 
   const handleAddNewTodo = () => {
     const newTodoList = [...todos];
-    newTodoList.push({ title: newTodo });
+    newTodoList.push({ title: newTodo, isCompleted: false });
     setTodos(newTodoList);
   };
 
